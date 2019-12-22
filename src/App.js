a92865c05de539d2e939bab4ebde86df2497c571
Lycosh <Lycosh211@gmx.de>
2019-12-22 20:11:52 +0100

    Make navigation sticky

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "react-bulma-components/dist/react-bulma-components.min.css";
 
 import Home from "./Components/Home";
 import Navigation from "Components/Navigation";
+import Timeline from "Components/Timeline";
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
       <Home semester="third"></Home>
+      <Timeline></Timeline>
     </div>
   );
 }
