e118fa64af2f4c383c1200bb472e4894d1686516
Lycosh <Lycosh211@gmx.de>
2020-01-02 10:09:52 +0100

    Add empty timeline handling

--- ClientApp/src/Components/Timeline.jsx
+++ ClientApp/src/Components/Timeline.jsx
@@ -2,6 +2,7 @@ import "./Timeline.scss";
 import Section from "react-bulma-components/lib/components/section";
 import Heading from "react-bulma-components/lib/components/heading";
 import Container from "react-bulma-components/lib/components/container";
+import Notification from "react-bulma-components/lib/components/notification";
 import TimelineItem from "./TimelineItem";
 
 import React, { Component } from "react";
@@ -15,10 +16,15 @@ class Timeline extends Component {
       <Section id="timeline" size="large">
         <Container>
           <Heading>Timeline</Heading>
-          {this.props.items &&
+          {this.props.items ? (
             this.props.items.map(item => {
               return <TimelineItem item={item} key={item.title} />;
-            })}
+            })
+          ) : (
+            <Notification className="is-danger">
+              Something went wrong while loading the timeline!
+            </Notification>
+          )}
         </Container>
       </Section>
     );
